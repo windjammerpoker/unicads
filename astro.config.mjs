9ef8b937124a1c78994003eb5a3b79328816d620
windjammerpoker <windjammerpoker@gmail.com>
2025-12-31 01:42:12 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://uncads.net",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
